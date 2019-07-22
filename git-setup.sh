8d1ceb328d714b6605a51d6fb3b990618d2c269d
Krzysztof Kapusta <piro91.pl@gmail.com>
2019-07-22 12:04:52 +0200

    [git] Autocorrect commands in git

--- git-setup.sh
+++ git-setup.sh
@@ -6,8 +6,8 @@ read -p "User email: " USEREMAIL
 git config --global user.name ${USERNAME-'Krzysztof Kapusta'}
 git config --global user.email ${USEREMAIL-'krz.kapusta@gmail.com'}
 git config --global log.follow true
+git config --global help.autocorrect 1
 
-git config --global alias.statsu status
 git config --global alias.tree "log --graph --color --decorate --format=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --branches --remotes"
 git config --global alias.htree '!git tree | head -20'
 git config --global alias.reorder 'rebase -i origin/HEAD'
